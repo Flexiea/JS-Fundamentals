32199c8d19d609fc35f98b2167a08d266021ccee
Kayode Adebisi <Flexieamiles@gmail.com>
2025-09-20 06:20:36 +0100

    Add factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,14 @@
+function factorial (args){
+   let result = 1
+    if(args >0){
+        return undefined
+    }
+    else if (args===NaN){
+        return result
+    }
+
+    for (let i=0;1>arg;1++){
+        args=args[i]
+    }
+
+}
--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 
-
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (const line of lines) {
